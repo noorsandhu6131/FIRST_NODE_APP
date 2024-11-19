4ff80772f98cd5c345164a282966d4f9332a72e4
Harnoor <noorsandhu6131@gmail.com>
2024-11-18 19:09:51 -0700

    First Update

--- app.js
+++ app.js
@@ -2,9 +2,31 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+app.use((req,res,next)=>{
+    console.log(`Request recieved:${req.method}, ${req.url}`);
+    next();
+})
+
+
+app.get("/user/:id",(req, res)  =>{
+    const userId = req.params.id; //dot notation
+   res.send(`User id is ${userId}`);
+})
+
+app.get("/search",(req, res)  =>{
+    const query = req.query.q; 
+   res.send(`Search query is ${query}`);
+})
+
 app.get('/', (req, res)=>{
     res.send("Hello World!");
 });
+
+
+
+
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
